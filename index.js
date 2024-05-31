67a1edf080e46e9c5626a3ea71e21d70b5ec3de1
botprzemek <info@botprzemek.pl>
2024-05-31 06:09:10 +0200

    feat(app): added clock controls

--- index.js
+++ index.js
@@ -18,22 +18,60 @@ class Timer {
     quarter = 0;
 
     constructor() {
-        this.quarter = config.getQuarterAmount();
-        this.set(document.querySelector(".quarter_number"), this.quarter);
-
+        this.quarter = 1;
         this.seconds = config.getQuarterLength();
-        this.set(document.querySelector(".minutes"), this.seconds);
 
-        this.start();
-        // document.querySelector(".control").addEventListener("click", () => {
-        //
-        // });
+        const [ minutes, seconds ] = this.format();
+        this.set(document.querySelector(".timer .minutes"), minutes);
+        this.set(document.querySelector(".timer .seconds"), seconds);
+        this.set(document.querySelector(".quarter .number"), this.quarter);
+        this.set(document.querySelector(".timer .control"), "Start Clock");
+
+        document.querySelector(".timer .control").removeAttribute("disabled");
+
+        document.querySelector(".timer .control").addEventListener("click", () => {
+            this.toggle();
+        });
+
+        document.querySelector(".timer .minutes_up").addEventListener("click", () => {
+            this.seconds += 60;
+
+            const [ minutes, seconds ] = this.format();
+            this.set(document.querySelector(".timer .minutes"), minutes);
+            this.set(document.querySelector(".timer .seconds"), seconds);
+        });
+
+        document.querySelector(".timer .minutes_down").addEventListener("click", () => {
+            this.seconds -= 60;
+
+            const [ minutes, seconds ] = this.format();
+            this.set(document.querySelector(".timer .minutes"), minutes);
+            this.set(document.querySelector(".timer .seconds"), seconds);
+        });
+
+        document.querySelector(".timer .seconds_up").addEventListener("click", () => {
+            this.seconds++;
+
+            const [ minutes, seconds ] = this.format();
+            this.set(document.querySelector(".timer .minutes"), minutes);
+            this.set(document.querySelector(".timer .seconds"), seconds);
+        });
+
+        document.querySelector(".timer .seconds_down").addEventListener("click", () => {
+            this.seconds--;
+
+            const [ minutes, seconds ] = this.format();
+            this.set(document.querySelector(".timer .minutes"), minutes);
+            this.set(document.querySelector(".timer .seconds"), seconds);
+        });
     }
 
     run = () => {
         if (this.seconds !== 0) {
             this.seconds--;
-            this.set(document.querySelector(".minutes"), this.format());
+            const [ minutes, seconds ] = this.format();
+            this.set(document.querySelector(".timer .minutes"), minutes);
+            this.set(document.querySelector(".timer .seconds"), seconds);
 
             return;
         }
@@ -52,29 +90,42 @@ class Timer {
         element.innerText = value;
     };
     start = () => {
-        this.seconds = config.getQuarterLength();
-        this.set(document.querySelector(".minutes"), this.seconds);
+        const [ minutes, seconds ] = this.format();
+        this.set(document.querySelector(".timer .minutes"), minutes);
+        this.set(document.querySelector(".timer .seconds"), seconds);
 
         this.timer = setInterval(this.run, 1000);
     };
-    next = () => {
-        if (this.quarter <= 1) {
+    toggle = () => {
+        if (this.timer !== null) {
+            this.stop();
+            this.set(document.querySelector(".timer .control"), "Start Clock");
+
             return;
-        };
+        }
+
+        this.start();
+        this.set(document.querySelector(".timer .control"), "Stop Clock");
+    };
+    next = () => {
+        if (this.quarter <= config.getQuarterAmount()) {
+            return;
+        }
         
-        this.quarter--;
-        this.set(document.querySelector(".quarter_number"), this.quarter);
+        this.quarter++;
+        this.set(document.querySelector(".quarter .number"), this.quarter);
 
         this.start();
     };
     stop = () => {
         clearInterval(this.timer);
+
+        this.timer = null;
     };
-    format() {
-        const minutes = Math.floor(this.seconds / 60);
-        const remainingSeconds = this.seconds % 60;
-        return `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
-    };
+    format = () => [
+        String(Math.floor(this.seconds / 60)).padStart(2, "0"),
+        String(this.seconds % 60).padStart(2, "0")
+    ];
 }
 
 const config = new Config();
